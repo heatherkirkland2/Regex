d386e7dc6ddaf599ad1171d03a0166e41a51c472
Heather Kirkland <heatherkirkland2@gmail.com>
2024-07-25 12:25:57 -0700

    update

--- Python Regular Expressions Deep Dive.py
+++ Python Regular Expressions Deep Dive.py
@@ -16,15 +16,6 @@ Adapt the regex pattern to exclude email addresses from '[exclude.com](http://ex
 
 Ensure the script still extracts all other valid email addresses. 
 '''
-import re
-
-text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
-emails = re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", text)
-print(emails)
-
-
-
-
 
 import re
 
